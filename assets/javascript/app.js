f7dfbc79770f03bb0e465e61fd33d830a7a76b9f
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2019-08-18 11:20:05 -0700

    Created timer, counts down to zero and stops at zero

---
+++ assets/javascript/app.js
@@ -0,0 +1,101 @@
+
+
+var score = 0;
+
+var myQuestions = [
+    {
+      question: "Who is the strongest?",
+      answers: {
+        a: "Superman",
+        b: "The Terminator",
+        c: "Waluigi, obviously"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is the best site ever created?",
+      answers: {
+        a: "SitePoint",
+        b: "Simple Steps Code",
+        c: "Trick question; they're both the best"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Where is Waldo really?",
+      answers: {
+        a: "Antarctica",
+        b: "Exploring the Pacific Ocean",
+        c: "Sitting in a tree",
+        d: "Minding his own business, so stop asking"
+      },
+      correctAnswer: "d"
+    }
+  ];
+
+  // var startGame = function() {}
+
+  // for ( let i = 0; i < myQuestions.length; i++) {
+  //     var response = myQuestions[i];
+  //     if (response == questions[i].correctAnswer) {
+  //       score++;
+  //     }
+  //     else {
+  //       //wrong
+  //     }
+
+  // }
+
+  
+// This code will run as soon as the page loads
+window.onload = function() {
+    
+  $("#start").on("click", start);
+  
+};
+
+//  Variable that will hold our setInterval that runs the stopwatch
+var intervalId;
+
+// prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+var time = 5;
+
+function start() {
+
+  // DONE: Use setInterval to start the count here and set the clock to running.
+  if (!clockRunning) {
+    intervalId = setInterval(count, 1000);
+    clockRunning = true;
+    
+  }
+
+}
+
+function count() {
+
+  if ( time > 0) {
+    time--;
+    var converted = timeConverter(time);
+    console.log(converted);
+  
+    // DONE: Use the variable we just created to show the converted time in the "display" div.
+    $("#display").text("Time Remaining: " + converted + " seconds");
+  }
+  
+
+  
+}
+function timeConverter(t) {
+
+  var minutes = Math.floor(t / 5);
+  var seconds = t - (minutes * 5);
+  return seconds;
+}
+
+$("#score").text("Current Score: " + score + " points");
+
+
+
+
+
